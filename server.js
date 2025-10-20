76ca66bbd9c123af5087e8452988a92e5248384d
vovantutai <tutaivovan@gmail.com>
2025-10-21 01:15:09 +0700

    thêm cấu trúc cho db và thiết kế sơ bộ giao diện

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src", "views"));
 app.use(expressLayouts);
+app.set('layout extractLocals', true);
 //app.set("layout", "layout"); // layout mặc định: src/views/layout.ejs
 
 // Middleware
@@ -19,7 +20,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "src", "public")));
 app.use("/", homeRoutes);
+<<<<<<< Updated upstream
+=======
+// ======= Kết nối CSDL =======
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'quanlidatphong'
+};
+// ======= Tạo session store =======
+const sessionStore = new MySQLStore(dbConfig);
+// ======= Cấu hình session =======
+// ======= Cấu hình session =======
+app.use(
+  session({
+    key: 'thieunu_session',
+    secret: 'thieunu_secret_key',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2 // 2 tiếng
+    }
+  })
+);
+app.use((req, res, next) => {
+  res.locals.session = req.session || {};
+  next();
+});
 
+// ======= Cho phép dùng session trong EJS =======
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+>>>>>>> Stashed changes
 
 // ✅ Sử dụng routes
 app.use('/khachhang', khachhangRoutes);
